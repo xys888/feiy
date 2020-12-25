8fc450a35aa0e47a1cb5bf033795d6ba7ba377d7
xueyangsheng <xueyangsheng@9fbank.com.cn>
2020-12-25 10:04:08 +0800

    添加学生类，计算其数学，语文，英语三门成绩的平均数。

---
+++ src/Student.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Student {
+    private String name;
+    private Integer math;
+    private Integer english;
+    private Integer java;
+
+    public Student() {
+    }
+
+    public Student(String name, Integer math, Integer english, Integer java) {
+        this.name = name;
+        this.math = math;
+        this.english = english;
+        this.java = java;
+    }
+
+    private Integer avgTest(Integer math, Integer english, Integer java){
+        return (math+english+java)/3;
+    }
+
+    public static void main(String[] args) {
+while(true){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入学生姓名：");
+        String ss = sc.nextLine();
+        System.out.println("请输入数学成绩：");
+        int i1 = sc.nextInt();
+        System.out.println("请输入语文成绩：");
+        int i2 = sc.nextInt();
+        System.out.println("请输入英语成绩：" );
+        int i3 = sc.nextInt();
+        Student student=new Student();
+        if(ss !=null && ss.length()>0){
+            System.out.println("该学生： "+ss+"的平均成绩为："+student.avgTest(i1,i2,i3));
+        }else{
+            System.out.println("学生姓名不能为空，请输入学生姓名：");
+        }
+
+    }
+    }
+}
--- src/Test6.java
+++ src/Test6.java
@@ -5,13 +5,16 @@ public class Test6 {
 
     public static void main(String[] args) {
         String param = "445884865355710465 445884865355710465 435510758308970496 445614183443070977 445615581404594177";
-        String p = "445884865355700465";
+        String p = "435510758308970496";
         Map m = new HashMap();
+        int i = param.indexOf(p);
+        System.out.println("i"+":"+i);
         if(param.indexOf(p) != -1){
             m.put("settleType", "1");
         }else {
             m.put("settleType", "0");
         }
         System.out.println(m);
+        System.out.println(348431788.51-349078020.00);
     }
 }
