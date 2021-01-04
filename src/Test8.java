c3df9f928eb14ded7291de36db1a8d88dd716e2d
xueyangsheng <xueyangsheng@9fbank.com.cn>
2021-01-04 14:39:39 +0800

    增加取随机数的数组的绝对值的方式

---
+++ src/Test8.java
@@ -0,0 +1,19 @@
+
+public class Test8 {
+    public static void main(String[] args) {
+        double[] arr1 = new double[80];
+        for(int i =0;i<arr1.length;i++){
+            arr1[i]=(int)(Math.random()*1000)-500;
+        }
+        for(int k=0;k<20;k++){
+            System.out.println(arr1[k]);
+        }
+        int sum = 0;
+        for(int j=0;j<20;j++){
+            sum = (int) (sum+ arr1[j]);
+       }
+        System.out.println("==========================================================");
+        System.out.println(sum);
+        System.out.println(Math.abs(sum));
+    }
+}
